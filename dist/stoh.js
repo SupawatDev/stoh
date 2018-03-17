cb00347813cb0645b5408ebe672b9cfef1b3ee8b
Supawat <supawattt@gmail.com>
2018-03-17 23:04:16 +0100

    STOH 0.1.0 : first version of STOH for translation.

--- dist/stoh.js
+++ dist/stoh.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var fs = __importStar(require("fs"));
 var inputSpace = "";
 var inputFilename = "./example.st";
-var outputSpace;
+var outputSpace = "";
 var outputFilename = "./out/output.html";
 var variables = [];
 var remains = [];
@@ -19,24 +19,24 @@ var eye = "";
 function Read() {
     return fs.readFileSync(inputFilename);
 }
-function variablize(inputEye) {
+function variablize() {
     //signal
     //console.log("variablizing..");
     //initialize variable    
     var name = "";
     var text = "";
     //remove "var" and ";"
-    inputEye = inputEye.slice(4, -1);
+    eye = eye.slice(4, -1);
     //remove "space"
-    inputEye.trim();
+    eye = eye.trim();
     //begin collect the variable
     //myV = "hello world"
     var shortEye = "";
-    for (var i = 0; i < inputEye.length; i++) {
+    for (var i = 0; i < eye.length; i++) {
         //initialize shortEye
-        shortEye += inputEye[i];
+        shortEye += eye[i];
         //check from start until get "="
-        if (inputEye[i] === "=") {
+        if (eye[i] === "=") {
             //clean "myV =" to "myV"
             shortEye = shortEye.slice(0, -1);
             name = shortEye.trim();
@@ -44,7 +44,7 @@ function variablize(inputEye) {
             /*test*/
             //console.log(name+".");
         }
-        else if (inputEye[i] === "\"") {
+        else if (eye[i] === "\"") {
             shortEye = shortEye.trimLeft();
             if (shortEye.length > 1) {
                 text = shortEye.slice(1, -1);
@@ -62,33 +62,60 @@ function variablize(inputEye) {
     eye = "";
     variables.push({ vName: name, vText: text });
 }
-function tagilize(inputEye) {
-    inputEye = inputEye.trimLeft();
+function tagilize() {
+    eye = eye.trim();
     var tagName = "";
     var tagContent = "";
     var tagContentStartPos = 0;
+    var isOpenQuote = false;
+    var isOpenBracket = false;
     //get its content
-    for (var j = 0; j < inputEye.length; j++) {
+    for (var j = 0; j < eye.length; j++) {
         //check if there is atr in tag.
         //first tag"content"{} is presented to class.
         //ex div(id="element1",color="black"){}.
         //if { detected => assign the tag name to remain.
         //in case use ()
-        if (inputEye[j] === "(") {
+        if (eye[j] === "(") {
+            //set OpenBracket
+            isOpenBracket = true;
             //start get tag's name
-            tagName = inputEye.substring(0, j);
-            console.log('tagging:' + tagName);
+            tagName = eye.substring(0, j);
             tagContentStartPos = j + 1;
         }
-        else if (inputEye[j] === ")") {
+        else if (eye[j] === ")") {
             //check error
             if (tagContentStartPos === 0) {
                 console.log('error! put ( at first place');
-                return;
+                break;
             }
-            tagContent = inputEye.substring(tagContentStartPos, j);
+            isOpenBracket = false;
+            tagContent = eye.substring(tagContentStartPos, j);
+            break; //stop the function after get everything
         }
-        //
+        //in case use "" + preventing qouation from tag("")
+        if (isOpenBracket === false) {
+            if (eye[j] === "\"" && isOpenQuote === true) {
+                tagContent = "class =" + eye.slice(tagContentStartPos - 1, -1);
+                console.log(tagContent);
+                isOpenQuote = false;
+                break;
+            }
+            else if (eye[j] === "\"") {
+                //openQuote
+                isOpenQuote = true;
+                //start get tag's name
+                tagName = eye.substring(0, j);
+                //get startPos
+                tagContentStartPos = j + 1;
+                //console.log(tagName);
+            }
+        }
+        //in case without content
+        if (eye[j] === "{" && isOpenBracket === false && isOpenQuote == false) {
+            tagName = eye.substring(0, j);
+            break;
+        }
     }
     //delete eyes after finish.
     //input contents to outputSpace.
@@ -98,24 +125,42 @@ function tagilize(inputEye) {
         console.log("there's something wrong");
     }
     else {
-        outputSpace += "<" + tagName + " " + tagContent + ">";
+        if (tagContent !== "") {
+            outputSpace += '\t'.repeat(remains.length) + "<" + (tagName + " " + tagContent) + ">" + "\n";
+        }
+        else {
+            outputSpace += '\t'.repeat(remains.length) + ("<" + tagName + ">") + "\n";
+        }
+        //tell the remains.
         remains.push(tagName);
     }
 }
 //replacing after finishing outputSpace.
 function replaceVariables() {
     for (var k = 0; k < variables.length; k++) {
-        outputSpace.replace("&" + variables[k], variables[k].vText);
+        outputSpace = outputSpace.toString().replace("&" + variables[k].vName, variables[k].vText);
     }
 }
 function closingTags() {
-    //closing tag. ex </html>
-    if (remains.length === 1) {
-        outputSpace += "</" + remains[0] + ">";
+    //closing tag. ex </html>;
+    console.log(remains);
+    //check if ..} is blank 
+    if (eye.indexOf(';') === -1 && eye.indexOf('&') === -1) {
+        outputSpace += "\t".repeat(remains.length - 1) + ("</" + remains[remains.length - 1] + ">");
+        remains = remains.slice(0, -1);
     }
     else {
-        outputSpace += "</" + remains[-1] + ">";
+        eye = eye.replace(/\s/g, '');
+        if (eye.indexOf('&') !== -1) {
+            outputSpace += "\t".repeat(remains.length) + eye.slice(0, eye.length - 1) + "\n";
+        }
+        else {
+            outputSpace += "\t".repeat(remains.length) + eye.slice(1, eye.length - 1) + "\n";
+        }
+        outputSpace += "\t".repeat(remains.length - 1) + ("</" + remains[remains.length - 1] + ">");
+        remains = remains.slice(0, -1);
     }
+    eye = "";
 }
 function Lex() {
     //loop file characters
@@ -125,20 +170,33 @@ function Lex() {
         //console.log('<--'+eye+'-->');
         //find variables
         if (eye.substring(0, 3) === "var" && inputSpace[i] === ";" /*TMS:ending var*/) {
-            variablize(eye);
+            variablize();
         }
         //Begin tagging when detect "{".
         if (inputSpace[i] === "{") {
-            tagilize(eye);
+            tagilize();
         }
         else if (inputSpace[i] == "}") {
+            //console.log(eye);
+            closingTags();
+            outputSpace += "\n";
         }
     }
-    console.log(outputSpace);
     replaceVariables();
+}
+function writeToHTML() {
+    var exfile = fs.createWriteStream(outputFilename);
+    exfile.once('open', function (fd) {
+        exfile.write(outputSpace);
+        exfile.end();
+    });
 }
 function onRun() {
     inputSpace = Read().toString();
     Lex();
+    //console.log(outputSpace);   
+    writeToHTML();
 }
+console.time();
 onRun();
+console.timeEnd();
--- src/stoh.ts
+++ src/stoh.ts
@@ -5,7 +5,7 @@ interface Variable {
 }
 let inputSpace: string = "";
 let inputFilename: string = "./example.st";
-let outputSpace: string;
+let outputSpace: string ="";
 let outputFilename: string = "./out/output.html";
 let variables: Variable[] = [];
 let remains: string[] = [];
@@ -15,24 +15,24 @@ let eye: string = "";
 function Read() {
     return fs.readFileSync(inputFilename);
 }
-function variablize(inputEye: string): void {
+function variablize(): void {
     //signal
     //console.log("variablizing..");
     //initialize variable    
     let name: string = "";
     let text: string = "";
     //remove "var" and ";"
-    inputEye = inputEye.slice(4, -1);
+    eye = eye.slice(4, -1);
     //remove "space"
-    inputEye.trim();
+    eye = eye.trim();
     //begin collect the variable
     //myV = "hello world"
     let shortEye: string = "";
-    for (let i = 0; i < inputEye.length; i++) {
+    for (let i = 0; i < eye.length; i++) {
         //initialize shortEye
-        shortEye += inputEye[i];
+        shortEye += eye[i];
         //check from start until get "="
-        if (inputEye[i] === "=") {
+        if (eye[i] === "=") {
             //clean "myV =" to "myV"
             shortEye = shortEye.slice(0, -1);
             name = shortEye.trim();
@@ -41,7 +41,7 @@ function variablize(inputEye: string): void {
             //console.log(name+".");
         }
         //open the quotation
-        else if (inputEye[i] === "\"") {
+        else if (eye[i] === "\"") {
             shortEye = shortEye.trimLeft();
             if (shortEye.length > 1) {
                 text = shortEye.slice(1, -1);
@@ -59,48 +59,79 @@ function variablize(inputEye: string): void {
     eye = "";
     variables.push({ vName: name, vText: text });
 }
-function tagilize(inputEye: string) {
-    inputEye = inputEye.trimLeft();
+function tagilize() {
+    eye = eye.trim();
     let tagName: string = "";
     let tagContent: string = "";
     let tagContentStartPos: number = 0;
+    let isOpenQuote: boolean = false;
+    let isOpenBracket: boolean = false;
     //get its content
-    for (let j = 0; j < inputEye.length; j++) {
+    for (let j = 0; j < eye.length; j++) {
         //check if there is atr in tag.
         //first tag"content"{} is presented to class.
         //ex div(id="element1",color="black"){}.
         //if { detected => assign the tag name to remain.
-
-
         //in case use ()
-        if (inputEye[j] === "(") {
+        if (eye[j] === "(") {
+            //set OpenBracket
+            isOpenBracket = true;
             //start get tag's name
-            tagName = inputEye.substring(0, j);
-            console.log('tagging:' + tagName);
-            tagContentStartPos = j + 1;
-        } else if (inputEye[j] === ")") {
+            tagName = eye.substring(0, j);
+            tagContentStartPos = j+1;
+        } else if (eye[j] === ")") {
             //check error
             if (tagContentStartPos === 0) {
                 console.log('error! put ( at first place');
-                return;
+                break;
             }
-            tagContent = inputEye.substring(tagContentStartPos, j);
+            isOpenBracket = false;
+            tagContent = eye.substring(tagContentStartPos, j);
+            break; //stop the function after get everything
         }
 
-        //
+        //in case use "" + preventing qouation from tag("")
+        if (isOpenBracket === false) {
+            if (eye[j] === "\"" && isOpenQuote === true) {
+                tagContent = "class ="+eye.slice(tagContentStartPos-1,-1);
+                console.log(tagContent);
+                isOpenQuote=false;
+                break;
+            } else if (eye[j] === "\"") {
+                //openQuote
+                isOpenQuote = true;
+                //start get tag's name
+                tagName = eye.substring(0, j)
+                //get startPos
+                tagContentStartPos=j+1;
+                //console.log(tagName);
+            }
+
+
+        }
+
+
+        //in case without content
+        if(eye[j]==="{"&&isOpenBracket===false&&isOpenQuote==false){
+            tagName = eye.substring(0,j);
+            break;
+        }
 
     }
-
-
     //delete eyes after finish.
     //input contents to outputSpace.
     //input the remain.
     eye = "";
-
     if (tagName === "") {
         console.log("there's something wrong");
     } else {
-        outputSpace += `<${tagName} ${tagContent}>`;
+        if(tagContent!==""){
+        outputSpace += '\t'.repeat(remains.length) +"<"+`${tagName} ${tagContent}`+">" +"\n";
+        }else{
+            outputSpace += '\t'.repeat(remains.length)+`<${tagName}>` +"\n";
+        }
+
+        //tell the remains.
         remains.push(tagName);
     }
 }
@@ -108,16 +139,29 @@ function tagilize(inputEye: string) {
 //replacing after finishing outputSpace.
 function replaceVariables() {
     for (let k = 0; k < variables.length; k++) {
-        outputSpace.replace(`&${variables[k]}`, variables[k].vText);
+        outputSpace=outputSpace.toString().replace(`&${variables[k].vName}`, variables[k].vText);
     }
 }
 function closingTags() {
-    //closing tag. ex </html>
-    if (remains.length === 1) {
-        outputSpace += `</${remains[0]}>`;
-    } else {
-        outputSpace += `</${remains[-1]}>`;
+    //closing tag. ex </html>;
+    console.log(remains);
+    //check if ..} is blank 
+    if(eye.indexOf(';')===-1&&eye.indexOf('&')===-1){
+        outputSpace+="\t".repeat(remains.length-1)+`</${remains[remains.length-1]}>`;
+        remains=remains.slice(0,-1);
     }
+    //for 
+    else{
+        eye=eye.replace(/\s/g,'');
+        if(eye.indexOf('&')!==-1){
+            outputSpace+="\t".repeat(remains.length)+eye.slice(0,eye.length-1)+"\n";
+        }else{
+            outputSpace+="\t".repeat(remains.length)+eye.slice(1,eye.length-1)+"\n";
+        }
+        outputSpace+="\t".repeat(remains.length-1)+`</${remains[remains.length-1]}>`;
+        remains=remains.slice(0,-1);
+    }
+    eye="";
 }
 function Lex(): void {
     //loop file characters
@@ -128,23 +172,31 @@ function Lex(): void {
 
         //find variables
         if (eye.substring(0, 3) === "var" && inputSpace[i] === ";"/*TMS:ending var*/) {
-            variablize(eye);
+            variablize();
         }
         //Begin tagging when detect "{".
         if (inputSpace[i] === "{") {
-            tagilize(eye);
+            tagilize();
         } else if (inputSpace[i] == "}") {
-
+            //console.log(eye);
+            closingTags();
+            outputSpace+="\n";
         }
 
     }
 
-    console.log(outputSpace);
     replaceVariables();
 }
 
 
-
+function writeToHTML(){
+    const exfile = fs.createWriteStream(outputFilename);
+    exfile.once('open',function(fd){
+        exfile.write(outputSpace);
+        exfile.end();
+    }
+    );
+}
 
 
 
@@ -152,8 +204,11 @@ function Lex(): void {
 function onRun(): void {
     inputSpace = Read().toString();
     Lex();
+    //console.log(outputSpace);   
+    writeToHTML();
 }
 
 
-
+console.time();
 onRun();
+console.timeEnd();
